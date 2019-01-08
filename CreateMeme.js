5f2f351632cb0d78f7afa082d3737f092f98f16d
Cynthia <ivyc8192@gmail.com>
2019-01-07 22:40:03 -0800

    before the remove function

--- CreateMeme.js
+++ CreateMeme.js
@@ -4,25 +4,33 @@ document.addEventListener("DOMContentLoaded", function(){
         var container= document.createElement('div');
         container.setAttribute("id","memeContainer");
         container.setAttribute("class","meme");
-        var photoURL=document.querySelector("#URL").value;
-        var uppertext=document.querySelector("#texttop").value;
-        var lowertext=document.querySelector("#textbottom").value;
+        var photoURL=document.querySelector("#URL");
+        var uppertext=document.querySelector("#texttop");
+        var lowertext=document.querySelector("#textbottom");
         alert(photoURL);
         var upp= document.createElement("span");
-        upp.innerText= uppertext;
+        upp.innerText= uppertext.value;
         upp.setAttribute("id","top");
         var newPhoto = document.createElement("img");
-        newPhoto.setAttribute("src",photoURL);
+        newPhoto.setAttribute("src",photoURL.value);
         newPhoto.setAttribute("alt", "sorry, something went wront!");
         newPhoto.setAttribute("id", "memephoto")
         newPhoto.setAttribute("class", "meme");
         var bottom= document.createElement("span");
-        bottom.innerText= lowertext;
+        bottom.innerText= lowertext.value;
         bottom.setAttribute("id", "bottom")
-        var body=document.querySelector("body");
+        var removex = document.createElement("img");
+        removex.setAttribute("src", "http://cdn.onlinewebfonts.com/svg/img_439489.png");
+        removex.setAttribute("id", "removex");
         container.appendChild(newPhoto);        
         container.appendChild(upp);
         container.appendChild(bottom);
+        container.appendChild(removex); 
+        var body=document.querySelector("body");
         body.appendChild(container);
+        photoURL.value = '';
+        uppertext.value = '';
+        lowertext.value = '';
+
     });
 });
